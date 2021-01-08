1917fb5bc143e0b4433b93f8c39bc1154614c2c8
Chris Mague <github@mague.com>
2021-01-08 12:45:14 -0800

    start scrape script

---
+++ Cloud/scrape_cloud_logs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import requests
+import yaml
+import getopt
+import sys
+
+configfile = "./config.yaml"
+
+options, z = getopt.getopt(sys.argv[1:], 'c:h', ['config=',"help"])
+for opt, arg in options:
+    if opt in ('-h', '--help'):
+        print("HELP GOES HERE")
+    if opt in ('-c', '--config'):
+        configfile = arg
+    
+
+try:
+    with open(configfile) as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+except Exception as err:
+    print("Unable to load config file: %s" %(configfile))
+    exit(1)
+
+
+
+url = "https://%s/logs/endpoint" % (config['host'])
+
+payload = {'limit': 200, 'offset': 0}
+
+headers = {
+    'user-agent': 'logstash-scraper',
+    'accept': 'application/json',
+    'x-api-key': config['api-key'],
+    'x-api-secret-key': config['api-secret'],
+    }
+try:
+    r = requests.get(
+        url,
+        headers=headers,
+        params=payload
+        )
+
+    if r.status_code != 200:
+        print("ERROR: HTTP status code: %s for %s" % (r.status_code, config['host']))
+        exit(1)
+
+except Exception as err:
+    print("ERROR: ", err)
+    exit(1)
