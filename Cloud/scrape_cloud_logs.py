0e492931dd4d38619625f9434fce369f836c503e
Chris Mague <github@mague.com>
2021-01-11 11:28:24 -0800

    working script

--- Cloud/scrape_cloud_logs.py
+++ Cloud/scrape_cloud_logs.py
@@ -6,16 +6,28 @@ import getopt
 import sys
 import datetime
 import json
-
+import time
 
 def fetch_data(config):
+
+    try:
+        logfile = open(config['log-file'], 'a')
+    except Exception as err:
+        print("Unable to open logfile: ", config['log-file'])
+        exit(1)
+
     offset = 0
     try:
         f = open(config['state-file'], 'r')
         offset = int(f.read().rstrip())
         f.close()
     except Exception as err:
-        print("no state file exists")
+        logfile.write(json.dumps(
+            {"time": int(datetime.datetime.now().timestamp()),
+            "type": "Info",
+            "description": "Writing new statefile {}".format(config['state-file'])}
+            ))
+        logfile.write("\n")
 
     url = "https://%s/v1/logs" % (config['host'])
 
@@ -35,19 +47,31 @@ def fetch_data(config):
             )
 
         if r.status_code != 200:
-            print("ERROR: HTTP status code: %s for %s" % (r.status_code, config['host']))
-            exit(1)
+            logfile.write(json.dumps(
+                {"time": int(datetime.datetime.now().timestamp()),
+                "type": "Error",
+                "description": "ERROR: HTTP status code: {} for {}".format(r.status_code, config['host'])}
+                ))
+            logfile.write("\n")
+            logfile.close()
+            return
 
         j = r.json()
 
         for entry in sorted(j['entries'], key=lambda item: item['id']):
             entry['time'] = int(datetime.datetime.strptime(entry['time'], "%Y-%m-%dT%H:%M:%SZ").strftime('%s'))
             if entry['time'] >= offset:
-                print(json.dumps(entry))
+                logfile.write(json.dumps(entry))
+                logfile.write("\n")
                 offset = entry['time']
 
     except Exception as err:
-        print("ERROR: ", err)
+        logfile.write(json.dumps(
+            {"time": int(datetime.datetime.now().timestamp()),
+            "type": "Error",
+            "description": err}
+            ))
+        logfile.write("\n")
 
 
     try:
@@ -55,7 +79,14 @@ def fetch_data(config):
         w.write(str(offset+1))
         w.close()
     except Exception as err:
-        print("ERROR: ", err)
+        logfile.write(json.dumps(
+            {"time": int(datetime.datetime.now().timestamp()),
+            "type": "Error",
+            "description": err}
+            ))
+        logfile.write("\n")
+
+    logfile.close()
 
 if __name__ == "__main__":
 
@@ -64,7 +95,8 @@ if __name__ == "__main__":
     options, z = getopt.getopt(sys.argv[1:], 'c:h', ['config=',"help"])
     for opt, arg in options:
         if opt in ('-h', '--help'):
-            print("HELP GOES HERE")
+            print("Run with -c or --config to specify a config file other than ./config.yaml")
+            exit
         if opt in ('-c', '--config'):
             configfile = arg
 
@@ -75,4 +107,6 @@ if __name__ == "__main__":
         print("Unable to load config file: %s" %(configfile))
         exit(1)
 
-    fetch_data(config)
+    while True:
+        fetch_data(config)
+        time.sleep(config['poll-interval'])
